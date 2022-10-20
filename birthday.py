5f877f07c096800140fb67cb2c527a85d6309d25
DBD18 <DBD18@pitt.edu>
2022-10-20 12:49:06 -0400

    Birthday Object
    Added basic birthday object to use

---
+++ birthday.py
@@ -0,0 +1,33 @@
+month = None
+day = None
+year = None
+
+def __init__(self, month, day, year):
+    self.month = month
+    self.day = day
+    self.year = year
+
+# TO STRING
+def __str__(self):
+    return self.month + "/" +  self.day + "/" + self.year
+
+# RETURNS MONTH, DAY, YEAR
+def get_data(self):
+    return self.month, self.day, self.year
+
+# ACCESSORS
+def get_month(self):
+    return self.month
+
+def get_day(self):
+    return self.day
+
+def get_year(self):
+    return self.year
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
